c3c535ab21ae3683d7a87b493726423b3ae4af4e
vanyamelikov <vanyamelikov@yandex.ru>
2017-10-26 18:21:00 +0300

    start

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=kidsbase',
-    'username' => 'root',
-    'password' => 'mysql',
-    'charset' => 'utf8',
-];
